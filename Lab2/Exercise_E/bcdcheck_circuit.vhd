cd8f14f0af1a2d431fe9b68d33d55eeb8da8d280
reversed_word <martin.lyra.97@gmail.com>
2017-04-05 13:12:26 +0200

    Wrapped if-s in process

--- Lab2/Exercise_E/bcdcheck_circuit.vhd
+++ Lab2/Exercise_E/bcdcheck_circuit.vhd
@@ -15,7 +15,7 @@ use ieee.std_logic_unsigned.all;
 
 entity bcdcheck_circuit is
   port(
-  i_x: in unsigned(3 downto 0); -- x3, x2, x1, x0
+  i_x: in std_Logic_vector(3 downto 0); -- x3, x2, x1, x0
 
   o_max: out std_logic;
   o_min: out std_logic;
@@ -27,24 +27,27 @@ end bcdcheck_circuit;
 
 architecture main_func of bcdcheck_circuit is
   begin
-    -- max = 1 if x = 9
-    if (i_x = 9) then
-      o_max <= '1';
-    end if;
-    -- min = 1 if x = 0
-    if (i_x = 0) then
-      o_min <= '1';
-    end if;
-    -- even = 1 if x is even (x%2 = 0)
-    if ((i_x mod 2) = 0) then
-      o_even <= '1';
-    end if;
-    -- lo3 = 1 if x < 3
-    if (i_x < 3) then
-      o_lo3 <= '1';
-    end if;
-    -- noBCD = 1 if x > 9
-    if (i_x > 9) then
-      o_noBCD <= '1';
-    end if;
+    p_MAIN : process(i_x)
+    begin
+      -- max = 1 if x = 9
+      if (i_x = 9) then
+        o_max <= '1';
+      end if;
+      -- min = 1 if x = 0
+      if (i_x = 0) then
+        o_min <= '1';
+      end if;
+      -- even = 1 if x is even (x%2 = 0)
+      if ((i_x mod 2) = 0) then
+        o_even <= '1';
+      end if;
+      -- lo3 = 1 if x < 3
+      if (i_x < 3) then
+        o_lo3 <= '1';
+      end if;
+      -- noBCD = 1 if x > 9
+      if (i_x > 9) then
+        o_noBCD <= '1';
+      end if;
+    end process p_MAIN
 end main_func;
